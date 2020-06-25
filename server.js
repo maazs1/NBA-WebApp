394be0e624744bd6c2678075f34307cc7718f6cf
Maaz Siddique <siddiqma98@gmail.com>
2020-06-24 20:48:35 -0600

    Added NBA_games.js, /api/NBA/Games is working. Will show the games here

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const games = require('./route/NBA_games');
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(3000)
+ const port = process.env.PORT || 3000;
+
+app.use('/api/NBA/Games', games);
+
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
